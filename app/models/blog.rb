07d86742682c6bc7581835085b562cd3db11a339
Igor Viblyy <igor.viblyy@gmail.com>
2018-01-27 01:49:27 +0200

    fixed blog image

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -16,7 +16,7 @@
 #
 
 class Blog < ApplicationRecord
-  has_attached_file :image, styles: { medium: '300x300>', thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: '300x300>', thumb: "100x100>", blog: "294x190>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   belongs_to :author
