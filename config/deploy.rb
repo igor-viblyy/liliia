ec2c050d57737bd62cd4c16eac252a70ec9435e2
Igor Viblyy <igor.viblyy@gmail.com>
2018-01-30 13:56:33 +0200

    fixed some error

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
-set :console_env, :production
+# set :console_env, :production
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", "config/secrets.yml"
