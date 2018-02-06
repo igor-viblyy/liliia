c307c2c7d9fa50a27e8a04c0a80df909c7a05439
Igor Viblyy <igor.viblyy@gmail.com>
2018-02-06 23:12:47 +0200

    fixed meta tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,20 @@ class ArticlesController < BaseController
     @adverts_right = Advertising.right
   end
 
-  def show;end
+  def show
+    set_meta_tags title: @article.title,
+                  site: 'Лілія',
+                  description: @article.body.html_safe,
+                  og: {
+                    locale: 'uk_UK',
+                    type: 'article',
+                    site_name: 'Лілія',
+                    image: @article.image.path(:medium),
+                    title: @article.title,
+                    url:  article_url(@article),
+                    description: @article.body.html_safe
+                  }
+  end
 
   private
     def set_article
